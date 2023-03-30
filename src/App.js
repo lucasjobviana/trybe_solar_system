29fc087df09ba5224882c5a49dcbdfcbeb16f7be
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-03-30 15:35:56 -0300

    Finalizando o segundo requisito.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <SolarSystem />
         <p>Sistema Solar</p>
-      </div >
+      </div>
 
     );
   }
