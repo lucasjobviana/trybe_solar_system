df1837c5d68ed57ec47700cd67d223ac45cd63ef
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-03-30 18:10:23 -0300

    Iniciando ultimo requisito.

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {
+          missions.map((p) => {
+            const n = p.name;
+            const y = p.year;
+            const c = p.country;
+            const d = p.destination;
+
+            return <MissionCard key={ n } name={ n } year={ y } country={ c } destination={ d } />;
+          })
+        }
       </div>
     );
   }
