468b62edca7ac6974a56c2f955a97bb09fc033b5
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-03-30 17:23:41 -0300

    Finalizando o sexto requisito.

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const pl = planets;
+    // data.map((x) => <Hello key={x.id}>{x}</Hello>);
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          pl.map((p) => {
+            const n = p.name;
+            const i = p.image;
+            return <PlanetCard key={ n } planetName={ n } planetImage={ i } />;
+          })
+        }
       </div>
     );
   }
